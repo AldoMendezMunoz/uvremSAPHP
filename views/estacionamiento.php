fbaf84b5d407407648f2b365140993092e740c68
Joaquin Marquez <j.marquez.p@outlook.es>
2019-07-08 11:26:17 -0400

    Varios puntos en el mapa

--- views/estacionamiento.php
+++ views/estacionamiento.php
@@ -22,6 +22,7 @@
     <link rel='stylesheet' href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v2.3.0/mapbox-gl-geocoder.css' type='text/css' />
 </head>
 <body>
+
     <?php include('../template/menuLogeado.php'); ?>
 
     <div>
@@ -35,9 +36,10 @@
                 <table class="table">
                     <tr>
                         <td class="text-center">Estacionamientos</td>
-                        <td class="text-center">Accion</td>
+                        <td class="text-center">Acción</td>
                     </tr>
-
+                    
+                    <!-- MOSTRAR DATOS EN LA TABLA -->
                     <?php 
                         $sql = "SELECT * FROM estacionamiento";
                         $result = mysqli_query($conexion, $sql);
@@ -66,7 +68,6 @@
 
     <script>
         mapboxgl.accessToken = 'pk.eyJ1IjoiY2hpbm9lbGN0bSIsImEiOiJjanhhazRnOGEwc252M3puMTk2empuMWo0In0.Nz9sUMpPCLcTv5VymZOWtw';
-
         
         var map = new mapboxgl.Map({
         container: 'map', // container id
@@ -79,7 +80,7 @@
         map.addControl(new MapboxGeocoder({
             accessToken: mapboxgl.accessToken
         }));
-        
+
         // obtener ubicacion
         map.addControl(new mapboxgl.NavigationControl());
         map.addControl(new mapboxgl.FullscreenControl());
@@ -89,20 +90,36 @@
             },
             trackUserLocation: true
         }));
-        
-
-        var element = document.createElement('div')
-        element.className = 'marker'
-
-        var marker = new mapboxgl.Marker(element)
-            .setLngLat({
-                lng: -70.7842389,
-                lat: -33.5393016
-            }).addTo(map);
 
     </script>
+    <!-- AGREGAR PUNTERO -->
+    <?php 
+        $sql = "SELECT * FROM estacionamiento";
+        $result = mysqli_query($conexion, $sql);
+        $count = mysqli_num_rows($result);
+        while($row = mysqli_fetch_array($result)){
+            echo "<script>\n";
+            echo "var marcador = '".$row[0]."'\n";
+            echo "var latitud = '".$row[5]."'\n";
+            echo "var longitud = '".$row[6]."'\n";
+            echo "</script>\n";
+    ?>
+    <script>
+        
+        marcador = document.createElement('div')
+        marcador.className = 'marker'
 
+        var marker = new mapboxgl.Marker(marcador)
+            .setLngLat({
+                lng: latitud,
+                lat: longitud
+            }).addTo(map);
+    </script>
+    <?php 
+        }
+    ?>
     <?php include('../template/footer.php'); ?>
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
